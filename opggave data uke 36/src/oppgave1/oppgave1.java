be0b7a293c7c316e102f429899b33ea00183de66
Brage01 <89256980+Brage01@users.noreply.github.com>
2021-09-10 01:27:40 +0200

    oppgave

--- opggave data uke 36/src/oppgave1/oppgave1.java
+++ opggave data uke 36/src/oppgave1/oppgave1.java
@@ -8,38 +8,38 @@ public class oppgave1 {
 	public static void main(String[] args) {
 		String tallTxt =	showInputDialog ("lønn");
 		double Lønn = Integer.parseInt(tallTxt);
-		double skattTrinn0;
-		double skattTrinn1;
-		double skattTrinn2;
-		double skattTrinn3;
+		double skattTrinn0 = 0;
+		double skattTrinn1 = 0;
+		double skattTrinn2 = 0;
+		double skattTrinn3 = 0;
 		double skattTrinn4 = 0;
 		
 		if(Lønn >=0 && Lønn <= 184800) 
 		{
 			skattTrinn0 = (Lønn - 0) * 0.000;
 		}
-		else {skattTrinn0 = (184800 - 0) * 0.000; }
+		if (Lønn > 184800) {skattTrinn0 = (184800 - 0) * 0.000; }
 		
 		if(Lønn > 184800 && Lønn <= 260100)
 		{
 			 skattTrinn1 = (Lønn - 184800) * 0.017;
 		}
 		
-		else {skattTrinn1 = (260100 - 184800) * 0.017; }
+		if (Lønn > 260100) {skattTrinn1 = (260100 - 184800) * 0.017; }
 		
 		if(Lønn > 260100 && Lønn <= 651250) 
 		{
 			 skattTrinn2 = (Lønn - 260100) * 0.040;
 		}
 		
-		else {skattTrinn2 = (651250 - 260100) * 0.040; }
+		if (Lønn > 651250) {skattTrinn2 = (651250 - 260100) * 0.040; }
 		
 		if(Lønn > 651250 && Lønn <= 1021550) 
 		{
 			skattTrinn3 = (Lønn - 651250) * 0.132;
 		
 		}
-		else {skattTrinn3 = (1021550 - 651250) * 0.132; }
+		if (Lønn > 1021550) {skattTrinn3 = (1021550 - 651250) * 0.132; }
 		
 		if(Lønn > 1021550) 
 		{
@@ -47,8 +47,8 @@ public class oppgave1 {
 		
 		}
 		
-		showMessageDialog(null, "skatt for de ulike trinnene er i synkende rekkefølge" + "\n" + 
-		skattTrinn0 + "kr" + "\n" + skattTrinn1 + "kr" + "\n" + skattTrinn2 + "kr" + "\n" + skattTrinn3 + "kr" + "\n" + skattTrinn4 + "kr");
+		showMessageDialog(null, "skatt for de ulike trinnene er i synkende rekkefølge" + "\n trinn0 \n" + 
+		skattTrinn0 + "kr" + "\n trinn1 \n" + skattTrinn1 + "kr" + "\n trinn2 \n" + skattTrinn2 + "kr" + "\n trinn3 \n" + skattTrinn3 + "kr" + "\n trinn4 \n" + skattTrinn4 + "kr");
 	}
 
 }
